5a0b2c914ca3ec8046dcc4b55f00078632b80925
umarsohail <umarsohail1998@gmail.com>
2024-03-31 15:56:46 +0500

    push

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import math
-import boto3
-import plotly.graph_objects as go
 import streamlit.components.v1 as components
-import numpy as np
-import os
-import json
 from sklearn import datasets
 iris = datasets.load_iris()
 mapping_dict = dict(zip(range(3), iris.target_names))  
@@ -26,8 +18,8 @@ st.markdown("<h3 style='text-align: center; color: black;'>USER INPUT </h3>",
 )
 col1, col2 = st.columns([1,1])
 
-# # -------- User inputs in columns 1 and 2 
-# # Sepal Length, Sepal Width, Petal Length and Petal Width
+# -------- User inputs in columns 1 and 2 
+# Sepal Length, Sepal Width, Petal Length and Petal Width
 sepal_length = col1.number_input(
     "Sepal Length (cm)",
     value=None,
@@ -38,71 +30,71 @@ sepal_length = col1.number_input(
     label_visibility="visible",
 )
 
-# sepal_width = col1.number_input(
-#     "Sepal Width (cm)",
-#     value=None,
-#     min_value=2.0,
-#     max_value=4.4,
-#     placeholder="Enter sepal width in cm...", 
-#     format="%d",
-#     label_visibility="visible",
-# )
+sepal_width = col1.number_input(
+    "Sepal Width (cm)",
+    value=None,
+    min_value=2.0,
+    max_value=4.4,
+    placeholder="Enter sepal width in cm...", 
+    format="%d",
+    label_visibility="visible",
+)
 
-# petal_length = col2.number_input(
-#     "Petal Length (cm)",
-#     value=None,
-#     min_value=1.0,
-#     max_value=6.9,
-#     placeholder="Enter petal length in cm...", 
-#     format="%d",
-#     label_visibility="visible",
-# )
+petal_length = col2.number_input(
+    "Petal Length (cm)",
+    value=None,
+    min_value=1.0,
+    max_value=6.9,
+    placeholder="Enter petal length in cm...", 
+    format="%d",
+    label_visibility="visible",
+)
 
-# petal_width = col2.number_input(
-#     "Petal Width (cm)",
-#     value=None,
-#     min_value=0.1,
-#     max_value=2.5,
-#     placeholder="Enter petal width in cm...", 
-#     format="%d",
-#     label_visibility="visible",
-# )
+petal_width = col2.number_input(
+    "Petal Width (cm)",
+    value=None,
+    min_value=0.1,
+    max_value=2.5,
+    placeholder="Enter petal width in cm...", 
+    format="%d",
+    label_visibility="visible",
+)
 
-# submit = st.button("Predict", help="Click here to start prediction", 
-#                     type="primary", use_container_width=True,
-# )
-# st.markdown("""<hr style="height:5px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
+submit = st.button("Predict", help="Click here to start prediction", 
+                    type="primary", use_container_width=True,
+)
+st.markdown("""<hr style="height:5px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
 
-# # ------- Prediction section 
-# st.markdown(
-#     "<h1 style='text-align: center; color: black;'>PREDICTION </h1>",
-#     unsafe_allow_html=True
-# )
+# ------- Prediction section 
+st.markdown(
+    "<h1 style='text-align: center; color: black;'>PREDICTION </h1>",
+    unsafe_allow_html=True
+)
 
-# if submit == True: 
-#     # Create a dictionary with the user's input values  
-#     user_input = {  
-#         "sepal_length": sepal_length,  
-#         "sepal_width": sepal_width,  
-#         "petal_length": petal_length,  
-#         "petal_width": petal_width,  
-#     }  
+if submit == True: 
+    # Create a dictionary with the user's input values  
+    user_input = {  
+        "sepal_length": sepal_length,  
+        "sepal_width": sepal_width,  
+        "petal_length": petal_length,  
+        "petal_width": petal_width,  
+    }  
   
-#     # Convert the dictionary to a JSON object  
+    # Convert the dictionary to a JSON object  
 #     user_input_json = json.dumps(user_input)  
   
-#     # st.write(user_input_json)   # Write to streamlit the input json if needed
+    # st.write(user_input_json)   # Write to streamlit the input json if needed
 
-#     # ---------------- TODO -----------------------
-#     # Pass the user_input_json to your trained model in the backend
-#     # Retrieve the prediction_json from the backend
-#     # Apply the mapping defined at the beginning to print the name of iris flower species
+    # ---------------- TODO -----------------------
+    # Pass the user_input_json to your trained model in the backend
+    # Retrieve the prediction_json from the backend
+    # Apply the mapping defined at the beginning to print the name of iris flower species
 
-#     prediction_json = {"prediction" : 1}  # dummy
-#     flower_name = mapping_dict[prediction_json['prediction']]
+    prediction_json = {"prediction" : 1}  # dummy
+    flower_name = mapping_dict[prediction_json['prediction']]
 
-#     # st.metric("Flower type", 
-#     #         str(flower_name),
-#     #         help="Flower type predicted by trained model", 
-#     #         label_visibility="visible",
-#     #         )  
+    st.metric("Flower type", 
+            str(flower_name),
+            help="Flower type predicted by trained model", 
+            label_visibility="visible",
+            )  
